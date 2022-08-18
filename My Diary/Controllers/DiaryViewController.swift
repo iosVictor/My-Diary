5efd41d939bd6b0cbffd602d4e004a1dd6d5a93e
victorKimpel() <mars.2874@gmail.com>
2022-08-19 01:50:39 +0300

    Добавление календаря Pod-файлом

--- My Diary/Controllers/DiaryViewController.swift
+++ My Diary/Controllers/DiaryViewController.swift
@@ -6,15 +6,25 @@
 //
 
 import UIKit
+import FSCalendar
 
 class DiaryViewController: UIViewController {
     
-    let datePicker: UIDatePicker = {
-        let picker = UIDatePicker()
-        picker.translatesAutoresizingMaskIntoConstraints = false
-        picker.preferredDatePickerStyle = .compact
-        picker.backgroundColor = #colorLiteral(red: 0.5791940689, green: 0.1280144453, blue: 0.5726861358, alpha: 0.5)
-        return picker
+    var calendarHeightConstraint: NSLayoutConstraint!
+    
+    private var calendar: FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        return calendar
+    }()
+    
+    let showHideButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Развернуть календарь", for: .normal)
+        button.setTitleColor(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1), for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir Next Demi Bold", size: 14)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
 
     override func viewDidLoad() {
@@ -23,27 +33,82 @@ class DiaryViewController: UIViewController {
         view.backgroundColor = .white
         title = "Дневник"
         
-        setupCalendar()
+        calendar.delegate = self
+        calendar.dataSource = self
+        calendar.scope = .week
+        
+        setConstraints()
+        swipeAction()
+        
+        showHideButton.addTarget(self, action: #selector(showHideButtonTapped), for: .touchUpInside)
     }
     
-    func setupCalendar() {
-        view.addSubview(datePicker)
+    @objc func showHideButtonTapped() {
+        if calendar.scope == .week {
+            calendar.setScope(.month, animated: true)
+            showHideButton.setTitle("Свернуть календарь", for: .normal)
+        } else {
+            calendar.setScope(.week, animated: true)
+            showHideButton.setTitle("Развернуть календарь", for: .normal)
+        }
+    }
+    
+    //MARK: Swipe
+    
+    func swipeAction() {
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeUp.direction = .up
+        calendar.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeDown.direction = .down
+        calendar.addGestureRecognizer(swipeDown)
+    }
+    
+    @objc func handleSwipe(gesture: UISwipeGestureRecognizer) {
+        switch gesture.direction {
+        case .up:
+            showHideButtonTapped()
+        case .down:
+            showHideButtonTapped()
+        default:
+            break
+        }
+    }
+}
+
+extension DiaryViewController: FSCalendarDataSource, FSCalendarDelegate {
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+}
+
+extension DiaryViewController {
+    
+    func setConstraints() {
+        view.addSubview(calendar)
+        
+        calendarHeightConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHeightConstraint)
+        
         NSLayoutConstraint.activate([
-            datePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
-//            datePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            datePicker.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 0),
-//            datePicker.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: 0)
+            calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
+            calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
         ])
         
-//        datePicker.addTarget(self, action: #selector(didTap), for: .touchUpInside)
+        view.addSubview(showHideButton)
+        NSLayoutConstraint.activate([
+            showHideButton.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant: 0),
+            showHideButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            showHideButton.widthAnchor.constraint(equalToConstant: 200),
+            showHideButton.heightAnchor.constraint(equalToConstant: 20)
+        ])
     }
-    
-    @objc private func didTap() {
-        let dateFormatter = DateFormatter()
-        let date = datePicker.date
-        dateFormatter.dateFormat = "MM-dd-yyyy HH:mm"
-        let string = dateFormatter.string(from: date)
-        print(string)
-    }
-    
 }
--- My Diary/Controllers/UIColorExtension.swift
+++ My Diary/Controllers/UIColorExtension.swift
@@ -9,12 +9,12 @@ import UIKit
 
 extension UIColor {
     static var tabBarItemAccent: UIColor {
-        #colorLiteral(red: 0.5791940689, green: 0.1280144453, blue: 0.5726861358, alpha: 1)
+        #colorLiteral(red: 0.7744978666, green: 0.2020181715, blue: 0.164677918, alpha: 1)
     }
     static var mainWhite: UIColor {
         #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.08697226821)
     }
     static var tabBarItemLight: UIColor {
-        #colorLiteral(red: 0.5791940689, green: 0.1280144453, blue: 0.5726861358, alpha: 0.3001862583)
+        #colorLiteral(red: 0.06314430386, green: 0.2748301029, blue: 0.8681097031, alpha: 1)
     }
 }
